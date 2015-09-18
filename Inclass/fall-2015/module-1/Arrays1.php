0e390a474fc77916ed75b36ee61b1bdb5598720d
KSH <simon.kim@utexas.edu>
2015-09-17 19:18:02 -0500

    Yayy

---
+++ Inclass/fall-2015/module-1/Arrays1.php
@@ -0,0 +1,35 @@
+<?php
+
+$pets = ['dog', 'cat', 'beast'];
+$flowers = ['rose', 'sunflower', 'mayflower'];
+
+
+// 1. Merge both these arrays together
+$pets_flowers = array_merge ($pets,$flowers);
+echo "<pre>";
+print_r ($pets_flowers);
+
+echo "<br>";
+
+// 2. Sort the arrays alphabetically
+sort ($pets_flowers);
+
+echo '<h3>Sorted</h3>';
+echo '<pre>';
+print_r ($pets_flowers);
+
+echo "<br>";
+
+// 3. Count the number of elements in the array
+$count = count ($pets_flowers);
+echo $count;
+
+echo "<br>";
+
+echo 'The array has '. count($pets_flowers) . ' elements' . PHP_EOL;
+
+?>
+
+
+
+
---
+++ Inclass/fall-2015/module-1/implode_explode.php
@@ -0,0 +1,14 @@
+<?php
+// 4. Create a comma separated string from the arrays
+//    using implode and explode
+
+$data = '"Samir", "Austin", "31", "Loves dogs", "Does not love cockroaches"';
+
+$dataArray = explode (",", $data); //explode breaks it a ","
+echo '<pre>';
+print_r($dataArray);
+
+$newData = array('coffee', 'juice', 'yerba mate'); //implode joins array with ".__."
+echo implode('.___.', $newData) . PHP_EOL;
+
+?>
