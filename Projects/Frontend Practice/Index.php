6b5761191584320bb062ba8fec3be81f11741121
KSH <simon.kim@utexas.edu>
2015-10-07 17:31:47 -0500

    frontend

---
+++ Projects/Frontend Practice/Index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Simon Kim's Portfolio</title>
+
+    <meta content="Simon Kim's Portfolio" name="title" />
+    <meta content="Practice Run" name="description" />
+<!--This is what people see when they google my page-->
+
+    <link rel="stylesheet" type="text/css" href="Basic.css">
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<!--Wrapper Class-->
+<div id="wrapper">
+    <nav> <!-- Top part -->
+        <a href="#" id="logo"><img src="logo.png" /></a>
+        <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+        </ul>
+        <a href="http://www.facebook.com" class="fb_link">Facebook</a>
+    </nav>
+
+    <div id="header" class="width_1000">
+        <p>Header</p>
+
+    </div>
+
+    <div class="clearfix"> <!-- adding some space -->
+    </div>
+
+
+    <div class="width_1000">
+        <div class="left_block">
+        <h1>LEFT</h1>
+            <p>I only want <span class="text-red">this word</span> to be red.</p>
+        </div>
+
+        <div class="right_block">
+        <h1>RIGHT</h1>
+        </div>
+    </div>
+
+    <div class="clearfix"> <!-- adding some space -->
+    </div>
+
+    <footer>
+    <p>footer</p>
+    <p>Say hello</p>
+
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+</div> <!-- end Wrapper -->
+
+
+</body>
+
+<?php
+?>
+
+</html>
